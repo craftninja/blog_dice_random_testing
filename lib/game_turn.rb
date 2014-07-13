c48921a487c4d42744c44685f9a30a4738c4228a
EmilyPlatzer <emilyplatzer@gmail.com>
2014-07-13 14:26:36 -0600

    User can roll and score for 1s

--- lib/game_turn.rb
+++ lib/game_turn.rb
@@ -1,9 +1,18 @@
+require_relative 'dice_roller'
+
 class GameTurn
 
   attr_reader :score
 
   def initialize
     @score = 0
+    @dice = DiceRoller.new
+  end
+
+  def roll(num_dice)
+    rolled = @dice.roll(num_dice).sort
+    points = rolled.count(1) * 100
+    "You rolled #{rolled} for #{points} points."
   end
 
 end
--- spec/game_turn_spec.rb
+++ spec/game_turn_spec.rb
@@ -9,4 +9,25 @@ describe GameTurn do
     expect(actual).to equal(expected)
   end
 
+  it 'allows user to roll dice and score them for the 1s' do
+    srand(118)
+    game_turn = GameTurn.new
+    actual = game_turn.roll(6)
+    expected = "You rolled [1, 2, 3, 4, 4, 6] for 100 points."
+    expect(actual).to eq(expected)
+    srand(Random.new_seed)
+  end
+
+  # it 'finds a certain roll' do
+  #   actual_roll = ""
+  #   i = 0
+  #   while actual_roll != "You rolled [1, 2, 3, 4, 4, 6] for 100 points."
+  #     srand(i)
+  #     game_turn = GameTurn.new
+  #     actual_roll = game_turn.roll(6)
+  #     p "srand #{i} - #{actual_roll}"
+  #     i+=1
+  #   end
+  # end
+
 end
